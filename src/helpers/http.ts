ff2e28364f97a9003e6472f8dfea1eccb02cfdf9
andregirao05 <andregirao05@gmail.com>
2023-05-02 16:23:26 -0400

    feat: add http helpers

---
+++ src/helpers/http.ts
@@ -0,0 +1,36 @@
+import { HttpResponse } from "../protocols";
+
+export function ok(data: any): HttpResponse<any> {
+  return {
+    statusCode: 200,
+    body: data,
+  };
+}
+
+export function noContent(error: Error | undefined): HttpResponse<any> {
+  return {
+    statusCode: 204,
+    body: error?.message,
+  };
+}
+
+export function badRequest(data: any): HttpResponse<any> {
+  return {
+    statusCode: 400,
+    body: data,
+  };
+}
+
+export function notFound(data: any): HttpResponse<any> {
+  return {
+    statusCode: 404,
+    body: data,
+  };
+}
+
+export function serverError(error: Error | undefined): HttpResponse<any> {
+  return {
+    statusCode: 500,
+    body: error?.message,
+  };
+}
