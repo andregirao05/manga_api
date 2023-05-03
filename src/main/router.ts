5645f5a96479b30d5f9c86993de9ea53ac8b90a0
andregirao05 <andregirao05@gmail.com>
2023-05-03 09:23:10 -0400

    feat: add route /mangas/updates/:origin

--- src/main/router.ts
+++ src/main/router.ts
@@ -3,14 +3,21 @@ import { adaptRoute } from "./routAdapt";
 import {
   makeGetMangaController,
   makeGetPopularMangasController,
+  makeGetLatestUpdatedMangasController,
 } from "./compositions";
 
 const router = Router();
 
 router.get("/mangas/:id", adaptRoute(makeGetMangaController()));
+
 router.get(
   "/mangas/populars/:origin",
   adaptRoute(makeGetPopularMangasController())
+);
+
+router.get(
+  "/mangas/updates/:origin",
+  adaptRoute(makeGetLatestUpdatedMangasController())
 );
 
 export { router };
