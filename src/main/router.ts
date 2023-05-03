4b75b498f8238b99b98aafc4ba6cd3bc5c4274e6
andregirao05 <andregirao05@gmail.com>
2023-05-03 09:28:00 -0400

    refactor: change module name from routAdapt to adaptRoute

--- src/main/router.ts
+++ src/main/router.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { adaptRoute } from "./routAdapt";
+import { adaptRoute } from "./adaptRoute";
 import {
   makeGetMangaController,
   makeGetPopularMangasController,
